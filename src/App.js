3c2c7cad251dbf1c0fed83bbb517f010b4d70087
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-13 11:01:57 +0530

    worked on feature branch

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Deploying app!!!
         </a>
       </header>
     </div>
