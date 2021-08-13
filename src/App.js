5a0360e394bc35f80308b05c4a80c97d9a239368
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-13 11:15:59 +0530

    final code

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Deploying app!!!
+          Learn React
         </a>
       </header>
     </div>
